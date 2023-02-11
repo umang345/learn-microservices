37db39d9f174144a816bead80c21fe6ce9c63c86
Umang <umangagarwal345@gmail.com>
2023-02-11 18:17:02 +0530

    added runner service for the sync communication rest template project

---
+++ runner-simulation-service-sync-resttemplate/runner-simulation-service-sync-resttemplate/src/main/java/com/umang345/runnersimulationservicesyncresttemplate/RunnerSimulationServiceSyncResttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.umang345.runnersimulationservicesyncresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerSimulationServiceSyncResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerSimulationServiceSyncResttemplateApplication.class, args);
+	}
+
+}
---
+++ runner-simulation-service-sync-resttemplate/runner-simulation-service-sync-resttemplate/src/test/java/com/umang345/runnersimulationservicesyncresttemplate/RunnerSimulationServiceSyncResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang345.runnersimulationservicesyncresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerSimulationServiceSyncResttemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

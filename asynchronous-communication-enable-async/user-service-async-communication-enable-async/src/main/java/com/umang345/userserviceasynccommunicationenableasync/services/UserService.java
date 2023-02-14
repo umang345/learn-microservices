cee2530583e7dbbb4406bf0ec5aaf87de68017d5
Umang <umangagarwal345@gmail.com>
2023-02-14 17:05:53 +0530

    Clean the code for user-service

--- asynchronous-communication-enable-async/user-service-async-communication-enable-async/src/main/java/com/umang345/userserviceasynccommunicationenableasync/services/UserService.java
+++ asynchronous-communication-enable-async/user-service-async-communication-enable-async/src/main/java/com/umang345/userserviceasynccommunicationenableasync/services/UserService.java
@@ -3,18 +3,9 @@ package com.umang345.userserviceasynccommunicationenableasync.services;
 import com.umang345.userserviceasynccommunicationenableasync.entities.User;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public interface UserService
 {
     User createUser(User newUser);
-
-//    User getUserById(long userId);
-//
-//    User updateUser(User user, long userId);
-//
-//    List<User> getAllUser();
-//
-//    void deleteUser(long userId);
 }
--- asynchronous-communication-enable-async/user-service-async-communication-enable-async/src/main/java/com/umang345/userserviceasynccommunicationenableasync/services/UserServiceImpl.java
+++ asynchronous-communication-enable-async/user-service-async-communication-enable-async/src/main/java/com/umang345/userserviceasynccommunicationenableasync/services/UserServiceImpl.java
@@ -1,12 +1,9 @@
 package com.umang345.userserviceasynccommunicationenableasync.services;
 
 import com.umang345.userserviceasynccommunicationenableasync.entities.User;
-import com.umang345.userserviceasynccommunicationenableasync.exceptions.ResourceNotFoundException;
 import com.umang345.userserviceasynccommunicationenableasync.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,54 +16,4 @@ public class UserServiceImpl implements UserService {
         User savedUser = userRepository.save(newUser);
         return savedUser;
     }
-
-//    @Override
-//    public User getUserById(long userId)  {
-//        User fetchedUser = null;
-//        try {
-//            fetchedUser = userRepository.findById(userId)
-//                    .orElseThrow(() -> new ResourceNotFoundException("User not found with id : "+userId));
-//        } catch (ResourceNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        return fetchedUser;
-//    }
-//
-//    @Override
-//    public User updateUser(User user, long userId) {
-//        User currentUser = null;
-//        try {
-//            currentUser = userRepository.findById(userId)
-//                    .orElseThrow(() -> new ResourceNotFoundException("User not found with id : "+userId));
-//            currentUser.setFirstName(user.getFirstName());
-//            currentUser.setLastName(user.getLastName());
-//            currentUser.setEmail(user.getEmail());
-//
-//        } catch (ResourceNotFoundException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//
-//        User updateUser = userRepository.save(currentUser);
-//        return updateUser;
-//    }
-//
-//    @Override
-//    public List<User> getAllUser() {
-//        List<User> users = userRepository.findAll();
-//        return users;
-//    }
-//
-//    @Override
-//    public void deleteUser(long userId) {
-//        User currentUser = null;
-//        try {
-//            currentUser = userRepository.findById(userId)
-//                    .orElseThrow(() -> new ResourceNotFoundException("User not found with id : "+userId));
-//        } catch (ResourceNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        userRepository.delete(currentUser);
-//    }
 }

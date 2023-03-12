baf2b15ced913caf0fe7fc704bb4fbaeabd3f447
Umang <umangagarwal345@gmail.com>
2023-03-13 01:51:00 +0530

    Added @LoadBalanced annotation for the Rest Template

--- service-discovery-eureka/runner-service/src/main/java/com/umang345/servicediscoveryeureka/runnerservice/configs/MyConfiguration.java
+++ service-discovery-eureka/runner-service/src/main/java/com/umang345/servicediscoveryeureka/runnerservice/configs/MyConfiguration.java
@@ -1,5 +1,6 @@
 package com.umang345.servicediscoveryeureka.runnerservice.configs;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class MyConfiguration
 {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
